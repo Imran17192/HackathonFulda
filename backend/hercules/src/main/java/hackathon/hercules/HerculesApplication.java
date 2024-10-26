d168f89fc50d9cb84dde3d8ac96f1856587dfd20
villmox <pderdorfbewohner@gmail.com>
2024-10-26 16:14:16 +0200

    added sql descriptions

--- backend/hercules/src/main/java/hackathon/hercules/HerculesApplication.java
+++ backend/hercules/src/main/java/hackathon/hercules/HerculesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HerculesApplication {
-
+    // insert
 	public static void main(String[] args) {
 		SpringApplication.run(HerculesApplication.class, args);
 	}
