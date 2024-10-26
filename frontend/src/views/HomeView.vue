d6fc83775c416347b1ac2ac5f913cd1187ddf884
MOx488 <116019653+MOx488@users.noreply.github.com>
2024-10-26 15:37:14 +0200

    add PostComponent

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
     import SearchBarComponent from "@/components/SearchBarComponent.vue";
+    import PostComponant from "@/components/PostComponant.vue";
+
 
 </script>
 
 <template>
     <SearchBarComponent />
+    <PostComponant />
 
 </template>
 
