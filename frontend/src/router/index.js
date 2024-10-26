15a7ad1bec324f496e183e70e84d73dab8df3859
Shivam Chaudhary <shivam.svm007@gmail.com>
2024-10-26 21:05:27 +0200

    Add template to show graph

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import SignupView from '@/views/auth/SignupView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
 import HomeView from "@/views/HomeView.vue";
 import PostView from "@/views/PostView.vue";
+import GraphView from '@/views/GraphView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,11 @@ const router = createRouter({
             path: '/new-post',
             name: 'new-post',
             component: PostView
+        },
+        {
+            path: '/graph',
+            name: 'graph',
+            component: GraphView
         },
         {
             path: '/:notFound(.*)',
---
+++ frontend/src/views/GraphView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GraphComponent from '@/components/GraphComponent.vue';
+
+</script>
+
+<template>
+    <GraphComponent />
+</template>
+
+<style scoped>
+
+</style>
